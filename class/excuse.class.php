f72769284bb9698acd34178f18810d7af4fe5281
orga-lc <lc@orga-buro.com>
2016-03-17 09:47:59 +0100

    Correction objet excuse

--- class/excuse.class.php
+++ class/excuse.class.php
@@ -6,7 +6,6 @@ class TExcuse extends TObjetStd {
 		
 		$this->set_table(MAIN_DB_PREFIX.'excuse');
 	  
-		$this->add_champs('fk_soc,fk_product',array('type'=>'integer', 'index'=>true));
 		$this->add_champs('heure','type=text;');
 		$this->add_champs('textExcuse','type=text;');
 		
